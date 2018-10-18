03e06862841b110c0ce5aff0ecb606d95bc496ec
shushuInBuaa <susan940910@hotmail.com>
2018-10-18 11:46:42 +0800

    new version

--- src/main/java/composition/ServiceCallable.java
+++ src/main/java/composition/ServiceCallable.java
@@ -19,7 +19,6 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
 
 import dataStructure.Service;
 import exception.InvalidServiceException;
